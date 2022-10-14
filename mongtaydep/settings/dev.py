e91c21f054e41d089342f8824086bf75cf143f73
Norman Viands <nlviands@gmail.com>
2022-10-13 20:34:40 -0400

    initial commit

---
+++ mongtaydep/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-rdu##55z3t9trln99r+@qtij2)@10q@p3xs$(_)e!e1&mq90a$"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
